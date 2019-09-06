080a7809ff7c79b5d7aea95ba7860e994ff0dc6a
Brendon Barnes <brenbrenofspace@gmail.com>
2019-09-06 07:14:02 -0400

    docs update and code condensing

---
+++ docs/README.md
@@ -0,0 +1,3 @@
+# PySets Version 0.5 Documentation
+
+`makeset()` - Takes one argument, either a string or array, which it returns as a set formatted as `Set X = {...}` (Roster Notation). **Note: When inputing a string, the string will be broken per 1 character, so `"test"` will return `{t, e, s}`!**
--- pysets.py
+++ pysets.py
@@ -1,26 +1,2 @@
-#this function takes a string and outputs a set in correct mathematical set notation, ex: Set A = {1, 2, 3}
-def stringtoset(tac):
-    firstlist = list(str(tac))
-    finallist = list(dict.fromkeys(firstlist))
-    standard = str.maketrans(dict.fromkeys("'[]"))
-    bold = str(finallist).translate(standard)
-    setNotation = ">>> Set X = {"+bold+"}"
-    return(setNotation)
-
-#this function returns only the part in brackets
-def stringtobold(tac):
-    firstlist = list(str(tac))
-    finallist = list(dict.fromkeys(firstlist))
-    standard = str.maketrans(dict.fromkeys("'[]"))
-    bold = str(finallist).translate(standard)
-    return(">>> {"+bold+"}")
-
-#this function takes a python array and returns a set in complete set notation
-def arraytoset(arr):
-    finallist = list(dict.fromkeys(arr))
-    standard = str.maketrans(dict.fromkeys(",'[] "))
-    bold = str(finallist).translate(standard)
-    arc = ", "
-    primobold = arc.join(bold)
-    setNotation = ">>> Set X = {"+primobold+"}"
-    return(setNotation)
+def makeset(arg):
+    return(">>> Set X = {"+", ".join(str(list(dict.fromkeys(arg))).translate(str.maketrans(dict.fromkeys(",'[] "))))+"}")
