5951faace0d6bd0877a344836b53bbeae9292097
Brendon Barnes <brenbrenofspace@gmail.com>
2019-09-09 07:18:24 -0400

    add `makesetArray()`

--- pysets.py
+++ pysets.py
@@ -1,2 +1,12 @@
+# pysets v0.5
+
+# this function takes an array or string and returns it as a human reable set
 def makeset(arg):
     return(">>> Set X = {"+", ".join(str(list(dict.fromkeys(arg))).translate(str.maketrans(dict.fromkeys(",'[] "))))+"}")
+
+# this function takes an array and returns an array that follows set rules
+def makesetArray(arg):
+    if type(arg) is arr:
+        return(list(dict.fromkeys(arg))
+    else:
+        raise TypeError("This function can only take an array.")
